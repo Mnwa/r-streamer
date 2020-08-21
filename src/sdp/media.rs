6e5804db5c02b9ff0ffdeb66b7ee899fd5b56ac9
Mnwa <mihan@panfilov.biz>
2020-08-21 13:51:44 +0300

    Optimize media list

--- src/sdp/media.rs
+++ src/sdp/media.rs
@@ -1,4 +1,5 @@
 use actix::Message;
+use bimap::BiMap;
 use std::collections::HashMap;
 use std::iter::FromIterator;
 use std::net::SocketAddr;
@@ -8,23 +9,20 @@ use webrtc_sdp::media_type::SdpMedia;
 
 pub type MediaUserStorage = HashMap<String, MediaList>;
 
-pub type MediaNamedMap = HashMap<String, u8>;
-pub type MediaMap = HashMap<u8, String>;
-
 #[derive(Debug, Clone, Default)]
-pub struct MediaList(MediaMap, MediaNamedMap);
+pub struct MediaList(BiMap<String, u8>);
 
 impl MediaList {
     pub fn get_name(&self, id: &u8) -> Option<&String> {
-        self.0.get(id)
+        self.0.get_by_right(id)
     }
-    pub fn get_id(&self, name: &str) -> Option<&u8> {
-        self.1.get(name)
+    #[allow(clippy::ptr_arg)]
+    pub fn get_id(&self, name: &String) -> Option<&u8> {
+        self.0.get_by_left(name)
     }
 
     pub fn insert(&mut self, id: u8, name: String) {
-        self.0.insert(id, name.clone());
-        self.1.insert(name, id);
+        self.0.insert(name, id);
     }
 }
 
